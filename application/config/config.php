e5a6bd37d84dfb4b257131bad48140a61c5d0bda
fedyakharisma <48073474+fedyakharisma@users.noreply.github.com>
2019-06-14 22:09:22 +0700

    cover

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://localhost/presensi_absen/';
+$config['base_url'] = 'http://localhost/presensi_absen1';
 
 /*
 |--------------------------------------------------------------------------
