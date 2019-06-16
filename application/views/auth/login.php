342d8f1e443567c785e09d8b9ecb68b42fe9482b
novitaayu <48073429+novitaayu@users.noreply.github.com>
2019-06-16 12:06:22 +0700

    login
    data dari registrasi untuk login

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -7,15 +7,16 @@
       <div class="card-header">Login Admin</div>
       <div class="card-body">
         <?= $this->session->flashdata('message'); ?>
-            <form class="user">
+         <form class="user" method="post" action="<?= base_url('CrudController/index'); ?>">
           <div class="form-group">
               <input type="text" id="email" class="form-control form-control-user" name="email" placeholder="Email address">
+              <?= form_error('email', '<small class="text-danger pl-3">', '</small>'); ?>
              
           </div>
           <div class="form-group">
            
               <input type="password" id="password" class="form-control form-control-user" name="password" placeholder="Password">
-             
+             <?= form_error('password', '<small class="text-danger pl-3">', '</small>'); ?>
           </div>
           
           <button type="submit" class="btn btn-primary btn-user btn-block">Login</button>
